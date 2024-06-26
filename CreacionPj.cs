2e2a305a1bf74bf58e7dc7d29187f08ab6b3461b
Targaaa <leotarga99@gmail.com>
2024-06-26 01:49:16 -0300

    terminando la fabrica y preparando api

--- CreacionPj.cs
+++ CreacionPj.cs
@@ -1,32 +1,54 @@
 namespace CreacionPj;
-    public enum Clases{ //es un listado que usare aleatoriamente para las clases de los personajes
-        Saber,
-        Archer,
-        Lancer,
-        Caster,
-        Assasin,
-        Berserker,
-        Rider,
-        Ruler
+    public class Personaje { 
+        private Datos datos;
+        private Caracteristicas caracteristicas;
+        public Personaje()
+        {
+            datos = new Datos();
+            caracteristicas = new Caracteristicas();
+        }
+        public Datos Datos { get => datos; set => datos = value; }
+        public Caracteristicas Caracteristicas { get => caracteristicas; set => caracteristicas = value; }
     }
 
-    public class Personaje { 
-        private int velocidad;
-        private int hp; //vida
+    public class Datos
+    {
+        private string className;
+        private string name;
+        private string story; // VER EN PROCESOOOOOOOOOOOOOOOOOOO  
+        private string noblePhantasms; //tesoro sagrado 
+        private string luck; //suerte
+        private string endurance;
+        private string magic;
+        private string agility;
+
+        public string ClassName { get => className; set => className = value; }
+        public string Name { get => name; set => name = value;}
+        public string Story { get => story; set => story = value; }
+        public string NoblePhantasms { get => noblePhantasms; set => noblePhantasms = value;}
+        public string Luck { get => luck; set => luck = value; }
+        public string Endurance { get => endurance; set => endurance = value; }
+        public string Magic { get => magic; set => magic = value; }
+        public string Agility { get => agility; set => agility = value; }
+} 
+public class Caracteristicas
+    {   
+        private double hpBase; //vida
+        private double atkBase; //ataque
         private int mp; //maná
         private int nivel; //el nivel ira aumentando conforme vaya avanzando de ronda en el torneo
-        private int sacred; //tesoro sagrado de los servants
-        private string clase;
-        private string nombre;
-        private string nacimiento;
-
-        public int Velocidad { get => velocidad;  set => velocidad = value; }
-        public int Hp { get => hp; set => hp = value; }       
+        private double defensa;
+        public Caracteristicas()
+        {
+            hpBase = 0;
+            atkBase = 0;
+            mp = 0;
+            nivel = 0;
+            defensa = 0;
+        }
         public int Mp { get=> mp; set => mp = value; }
         public int Nivel { get => nivel; set => nivel = value;}
-        public int Sacred { get=> sacred; set => sacred = value;}
-        public string Clase { get => clase; set => clase = value; }
-        public string Nombre { get => nombre; set => nombre = value;}
-        public string Nacimiento { get => nacimiento; set => nacimiento = value;}
-        
-} 
+        public double Defensa { get => defensa; set => defensa = value;}
+        public double AtkBase { get => atkBase; set => atkBase = value;}
+        public double HpBase{ get => hpBase; set => hpBase = value; }
+}
--- FabricaPersonajes.cs
+++ FabricaPersonajes.cs
@@ -1,6 +1,115 @@
+using System.Diagnostics;
+using Api;
 using CreacionPj;
+
 namespace Creaciones;
     public class CreacionPj{
-        
-        
+        public class Fabrica{
+            private static Random random = new Random();
+            public static List<Personaje> cargarDatos(List<Personaje> listaPersonajes, List<PersonajesApi> personajesApi){
+                foreach(var personaje in personajesApi){
+                    Personaje nuevoPersonaje = new Personaje();
+
+                    nuevoPersonaje.Datos.Name = personaje.Name;
+                    nuevoPersonaje.Datos.Endurance = personaje.Endurance;
+                    nuevoPersonaje.Datos.Agility = personaje.Agility;
+                    nuevoPersonaje.Datos.ClassName = personaje.ClassName;
+                    nuevoPersonaje.Datos.Luck = personaje.Luck;
+                    nuevoPersonaje.Datos.Magic = personaje.Magic;
+                    nuevoPersonaje.Datos.NoblePhantasms = personaje.NoblePhantasms;
+                    nuevoPersonaje.Datos.Story  = personaje.Story;
+
+                    switch (personaje.Luck){
+                        case "A++":
+                        case "A+":
+                        case "A":
+                            nuevoPersonaje.Caracteristicas.AtkBase = random.Next(10,41)*2.0;
+                        break;
+                        case "A-":
+                        case "A--":
+                            nuevoPersonaje.Caracteristicas.AtkBase = random.Next(10,41)*1.8;
+                        break;
+                        case "B++":
+                        case "B+":
+                        case "B":
+                            nuevoPersonaje.Caracteristicas.AtkBase = random.Next(10,41)*1.6;
+                        break;
+                        case "B-":
+                        case "B--":
+                            nuevoPersonaje.Caracteristicas.AtkBase = random.Next(10,41)*1.4;
+                        break;
+                        case "C++":
+                        case "C+":
+                        case "C":
+                            nuevoPersonaje.Caracteristicas.AtkBase = random.Next(10,41)*1.2;
+                        break;
+                        case "C-":
+                        case "C--":
+                            nuevoPersonaje.Caracteristicas.AtkBase = random.Next(10,41);
+                        break;
+                        case "D++":
+                        case "D+":
+                        case "D":
+                        case "D-":
+                        case "D--":
+                            nuevoPersonaje.Caracteristicas.AtkBase = random.Next(10,41)*0.8;
+                        break;
+                        case "E++":
+                        case "E+":
+                        case "E":
+                        case "E-":
+                        case "E--":
+                            nuevoPersonaje.Caracteristicas.AtkBase = random.Next(10,41)*0.6;
+                        break;
+                    }
+                    switch (personaje.Agility){
+                        case "A++":
+                        case "A+":
+                        case "A":
+                            nuevoPersonaje.Caracteristicas.Defensa = random.Next(10,41)*2.0;
+                        break;
+                        case "A-":
+                        case "A--":
+                            nuevoPersonaje.Caracteristicas.Defensa = random.Next(10,41)*1.8;
+                        break;
+                        case "B++":
+                        case "B+":
+                        case "B":
+                            nuevoPersonaje.Caracteristicas.Defensa = random.Next(10,41)*1.6;
+                        break;
+                        case "B-":
+                        case "B--":
+                            nuevoPersonaje.Caracteristicas.Defensa = random.Next(10,41)*1.4;
+                        break;
+                        case "C++":
+                        case "C+":
+                        case "C":
+                            nuevoPersonaje.Caracteristicas.Defensa = random.Next(10,41)*1.2;
+                        break;
+                        case "C-":
+                        case "C--":
+                            nuevoPersonaje.Caracteristicas.Defensa = random.Next(10,41);
+                        break;
+                        case "D++":
+                        case "D+":
+                        case "D":
+                        case "D-":
+                        case "D--":
+                            nuevoPersonaje.Caracteristicas.Defensa = random.Next(10,41)*0.8;
+                        break;
+                        case "E++":
+                        case "E+":
+                        case "E":
+                        case "E-":
+                        case "E--":
+                            nuevoPersonaje.Caracteristicas.Defensa = random.Next(10,41)*0.6;
+                        break;
+                    }
+                    nuevoPersonaje.Caracteristicas.Nivel= 1;
+                    
+                    listaPersonajes.Add(nuevoPersonaje);
+                }
+                return listaPersonajes;
+            }
+        }
     }
